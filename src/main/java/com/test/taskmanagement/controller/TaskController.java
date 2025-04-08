3a31c9ef372e21275377e6ed7e9cb8c2bf19784c
Pushkar <87086046+kkmayank98@users.noreply.github.com>
2025-04-09 03:58:22 +0530

    Pushed to Github

--- src/main/java/com/test/taskmanagement/controller/TaskController.java
+++ src/main/java/com/test/taskmanagement/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.test.taskmanagement.controller;
 import com.test.taskmanagement.model.Task;
 import com.test.taskmanagement.service.TaskService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,14 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @GetMapping
     public List<Task> getAllTask(){
         return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id){
+        return taskService.getTaskById(id);
     }
 
 
