30817cc3368af3df22dc7337d615e06bd8e48820
Pushkar <87086046+kkmayank98@users.noreply.github.com>
2025-04-09 03:54:26 +0530

    Pushed to Github

--- src/main/java/com/test/taskmanagement/controller/TaskController.java
+++ src/main/java/com/test/taskmanagement/controller/TaskController.java
@@ -1,11 +1,26 @@
 package com.test.taskmanagement.controller;
 
+import com.test.taskmanagement.model.Task;
+import com.test.taskmanagement.service.TaskService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    public List<Task> getAllTask(){
+        return taskService.getAllTasks();
+    }
 
 
 }
